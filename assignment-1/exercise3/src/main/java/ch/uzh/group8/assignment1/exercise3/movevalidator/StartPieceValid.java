2a2db5efca774aa451299a77ebd275544b9baf13
BacLuc <lucius.bachmann@gmx.ch>
2021-10-12 11:54:01 +0200

    implement StartPieceValid

--- assignment-1/exercise3/src/main/java/ch/uzh/group8/assignment1/exercise3/movevalidator/StartPieceValid.java
+++ assignment-1/exercise3/src/main/java/ch/uzh/group8/assignment1/exercise3/movevalidator/StartPieceValid.java
@@ -6,6 +6,10 @@ import ch.uzh.group8.assignment1.exercise3.Move;
 public class StartPieceValid implements MoveValidator {
   @Override
   public boolean validate(Move move, Board board) {
-    return true;
+    var pieceAt = board.getPieceAt(move.start());
+    if (pieceAt.isEmpty()) {
+      return false;
+    }
+    return move.player() == pieceAt.get().owner();
   }
 }
