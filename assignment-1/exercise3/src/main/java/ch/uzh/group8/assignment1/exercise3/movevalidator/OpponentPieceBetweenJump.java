177974291d827d332cea2cc9824345c0bc0cd9bf
BacLuc <lucius.bachmann@gmx.ch>
2021-10-12 12:33:28 +0200

    implement OpponentPieceBetweenJump
    Use the stubbing with specific coordinates to verify correct
    calculation of the coordinate between the jump.

--- assignment-1/exercise3/src/main/java/ch/uzh/group8/assignment1/exercise3/movevalidator/OpponentPieceBetweenJump.java
+++ assignment-1/exercise3/src/main/java/ch/uzh/group8/assignment1/exercise3/movevalidator/OpponentPieceBetweenJump.java
@@ -1,11 +1,45 @@
 package ch.uzh.group8.assignment1.exercise3.movevalidator;
 
+import static ch.uzh.group8.assignment1.exercise3.BoardCoordinates.*;
+
 import ch.uzh.group8.assignment1.exercise3.Board;
+import ch.uzh.group8.assignment1.exercise3.BoardCoordinates;
 import ch.uzh.group8.assignment1.exercise3.Move;
 
 public class OpponentPieceBetweenJump implements MoveValidator {
   @Override
   public boolean validate(Move move, Board board) {
-    return true;
+    var start = move.start();
+    var end = move.end();
+
+    var rowDiff = end.row().ordinal() - start.row().ordinal();
+    var colDiff = end.column().ordinal() - start.column().ordinal();
+
+    if (Math.abs(rowDiff) != 2 || Math.abs(colDiff) != 2) {
+      return true;
+    }
+
+    var rows = Row.values();
+    Row rowBetween;
+    if (rowDiff > 0) {
+      rowBetween = rows[start.row().ordinal() + 1];
+    } else {
+      rowBetween = rows[start.row().ordinal() - 1];
+    }
+
+    var columns = Column.values();
+    Column colBetween;
+    if (colDiff > 0) {
+      colBetween = columns[start.column().ordinal() + 1];
+    } else {
+      colBetween = columns[start.column().ordinal() - 1];
+    }
+
+    var pieceAt = board.getPieceAt(new BoardCoordinates(rowBetween, colBetween));
+    if (pieceAt.isEmpty()) {
+      return false;
+    }
+    var pieceBetween = pieceAt.get();
+    return pieceBetween.owner() != move.player();
   }
 }
